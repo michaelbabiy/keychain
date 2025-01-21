a6754c5ff69f9a92b3e99f4415faf75c950c4a04
Michael Babiy <michael.babiy@alaskaair.com>
2025-01-21 14:45:00 -0800

    Define settings.

--- Package.swift
+++ Package.swift
@@ -33,6 +33,7 @@ let package = Package(
             name: "KeychainAccess",
             path: "Framework/KeychainAccess",
             swiftSettings: [
+                .define("ENABLE_LIBRARY_EVOLUTION", .when(configuration: .release)),
                 .swiftLanguageMode(.v6)
             ]
         )
